6c31d3214abf41d847009407d83e6f4df685afd3
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-01-18 11:08:03 -0300

    requisito 3 concluido

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const listProduct = await fetchProductsList('computador');
+const products = document.querySelector('.products');
+listProduct.forEach((product) => {
+  products.appendChild(createProductElement(product));
+});
