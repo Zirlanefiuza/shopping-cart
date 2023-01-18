c259d87ad932130571500cc93e570d5675e25fac
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-01-18 13:15:10 -0300

    requisito 8 concluido

--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -1,4 +1,5 @@
-import { removeCartID } from './cartFunctions';
+import { removeCartID, saveCartID } from './cartFunctions';
+import { fetchProduct } from './fetchFunctions';
 
 // Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
@@ -121,6 +122,14 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
     'product__add',
     'Adicionar ao carrinho!',
   );
+  cartButton.addEventListener('click', async (event) => {
+    const product = event.target.parentNode;
+    const productId = getIdFromProduct(product);
+    saveCartID(productId);
+    const inf = await fetchProduct(productId);
+    const cart = document.querySelector('.cart__products');
+    cart.appendChild(createCartProductElement(inf));
+  });
   section.appendChild(cartButton);
 
   return section;
